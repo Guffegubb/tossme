d91927b6f0b3ae6e0fcd54de2fcf810aef581bf6
Gustaf <gustafolofsson93@gmail.com>
2016-09-15 09:42:15 +0000

    implemented high/long jump special abilities, locked movement while using abilities.

--- cricket/src/bodies/Player.js
+++ cricket/src/bodies/Player.js
@@ -3,65 +3,75 @@
 /*global Phaser*/
 
 Player = function(game, properties) {
-  
-  
-  
-  
-  
+
+
   this.player = game.add.sprite(properties.x, properties.y, 'player');
   this.player.anchor.setTo(0.5, 0.5);
   this.player.scale.setTo(0.5, 0.5);
-  
+
   game.physics.enable(this.player, Phaser.Physics.ARCADE);
-  
+
   this.player.body.collideWorldBounds = true;
   this.player.body.allowGravity = true;
-  
+
   this.player.playerSpeed = properties.playerSpeed;
   this.player.jumpHeight = properties.jumpHeight;
   this.player.abilityOne = 'highJump';
   this.player.abilityTwo = 'longJump';
-  
-  
-  
+
+  this.player.coolDown = 0;
+  this.player.coolDownTime = 2000;
+  this.player.moveLock = false;
+
+
   // TODO: Comment this function
   this.player.move = function(direction) {
 
     if (direction == "stop") {
-      player.body.velocity.x = 0;
+      if (player.body.velocity.x > 0)
+        player.body.velocity.x -= player.playerSpeed / 5;
+      else if (player.body.velocity.x < 0)
+        player.body.velocity.x += player.playerSpeed / 5;
+      if (player.body.onFloor())
+        player.setMoveLock(false);
     }
-    else if (direction == "right") {
-      player.scale.setTo(0.5, 0.5);
-      player.body.velocity.x += player.playerSpeed;
-    }
-    else if (direction == "left") {
-      player.scale.setTo(-0.5, 0.5);
-      player.body.velocity.x -= player.playerSpeed;
-    }
-    else if (direction == "jump") {
-      if (player.body.onFloor()) { // || player.body.touching.down
-        player.body.velocity.y -= player.jumpHeight;
+
+    if (!player.hasMoveLock()) {
+      if (direction == "right") {
+        player.scale.setTo(0.5, 0.5);
+        player.body.velocity.x = player.playerSpeed;
+      }
+      else if (direction == "left") {
+        player.scale.setTo(-0.5, 0.5);
+        player.body.velocity.x = -player.playerSpeed;
+      }
+      else if (direction == "jump") {
+        if (player.body.onFloor()) {
+          player.body.velocity.y -= player.jumpHeight;
+        }
+      }
+      else {
+        // TODO: Error handling  
       }
     }
-    else {
-      // TODO: Error handling  
-    }
-    
-  },
-  
-  // TODO: Implement timers so abilities can not be spammed.
+
+
+
+  };
+
+
   this.player.callAbility = function(key) {
-    
+
     var ability;
-    
+
     if (key == 1) {
       ability = player.abilityOne;
     }
     else if (key == 2) {
       ability = player.abilityTwo;
     }
-    
-    switch(ability) {
+
+    switch (ability) {
       case 'highJump':
         player.highJump();
         break;
@@ -76,26 +86,71 @@ Player = function(game, properties) {
         break;
       default:
     }
-    
-  },
-  
-  // TODO: Implement these
+
+  };
+
   this.player.highJump = function() {
-    console.log("jumping really high");
-  },
-  
+    if (!player.hasCoolDown() && player.body.onFloor()) {
+      this.duration = 0.6;
+      player.body.velocity.y -= player.jumpHeight * 2;
+
+      // This timer is used to create a smoother animation when using high jump
+      // movement in x-axis does not start until the player has reached a higher height.
+      game.time.events.add(Phaser.Timer.SECOND * this.duration, function() {
+        var playerTween = game.add.tween(player).to({
+          x: player.x + player.direction() * 150
+        }, 1000, 'Linear', true);
+
+      });
+
+      player.setCoolDown();
+      player.setMoveLock(true);
+    }
+
+  };
+
   this.player.longJump = function() {
-    console.log("jumping really long");
-  },
-  
+    // TODO: Do we want to have the body.onFloor condition here as well? 
+    if (!player.hasCoolDown()) {
+      player.body.velocity.y -= player.jumpHeight;
+      player.body.velocity.x = player.direction() * player.playerSpeed * 6;
+
+      player.setCoolDown();
+      player.setMoveLock(true);
+    }
+  };
+
+  // TODO: Implement these
   this.player.stomp = function() {
-    
-  },
-  
+
+  };
+
   this.player.shoot = function() {
-    
+
+  };
+
+  this.player.hasCoolDown = function() {
+    return !(game.time.now > player.coolDown);
+  };
+
+  this.player.hasMoveLock = function() {
+    return player.moveLock;
+  };
+
+  this.player.setCoolDown = function() {
+    player.coolDown = game.time.now + player.coolDownTime;
+  };
+
+  this.player.setMoveLock = function(bool) {
+    player.moveLock = bool;
+  };
+
+  this.player.direction = function() {
+    return (player.scale.x / Math.abs(player.scale.x));
   }
-  
+
+
+
   return this.player;
-  
+
 }
--- cricket/src/states/Level.js
+++ cricket/src/states/Level.js
@@ -1,4 +1,3 @@
-
 Game.Level = function(game) {};
 
 var map;
@@ -15,50 +14,50 @@ var coinReader;
 
 Game.Level.prototype = {
 
-    
+
     create: function(game) {
-        
+
         //
         coinReader = game.add.group();
         coinGroup = game.add.group();
         var singleCoin;
-        
-        
-        
+
+
+
         // Initialize map and tilesets
         this.stage.backgroundColor = '#3A5963';
-      
+
         map = this.add.tilemap('map');
         map.addTilesetImage('tileset', 'tileset');
         layer = map.createLayer('Tile Layer 1');
-        
+
         layer.resizeWorld();
         map.setCollisionBetween(0, 4);
-        
+
         map.createFromObjects('spawn', 8, '', 0, true, false, respawn);
         map.createFromObjects('coins', 7, '', 0, true, false, coinReader);
-        
+
         // the so called ful-lösning
         coinReader.forEach(function(item) {
             singleCoin = new Coin(game, item);
             coinGroup.add(singleCoin);
         }, this);
-        
-        
-        
-        
+
+
+
+
         this.physics.arcade.gravity.y = 1000;
         var playerProperties = {
             // TODO: implement without hardcoded number
-            x : 122,
-            y : 500,
-            playerSpeed : 150,
-            jumpHeight : 600,
+            x: 122,
+            y: 500,
+            playerSpeed: 200,
+            jumpHeight: 400,
         }
-        
+
         player = new Player(game, playerProperties);
         this.camera.follow(player);
-        
+
         controls = {
             right: this.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
             left: this.input.keyboard.addKey(Phaser.Keyboard.LEFT),
@@ -66,54 +65,55 @@ Game.Level.prototype = {
             abilityOne: this.input.keyboard.addKey(Phaser.Keyboard.Z),
             abilityTwo: this.input.keyboard.addKey(Phaser.Keyboard.X),
         };
-    
+
     },
-    
-    
+
+
     update: function(game) {
-        
+
         // Adding all collisions
         this.physics.arcade.collide(player, layer);
-        
+
         coinGroup.forEach(function(item) {
-            game.physics.arcade.overlap(player, item, function() { item.destroySprite() });
+            game.physics.arcade.overlap(player, item, function() {
+                item.destroySprite()
+            });
         }, this);
-        
-        
+
+
         // EndOf adding all collisions
-        
-        
+
+
         // Checking for player movement
-        
+
         player.move("stop");
-        
+
         if (controls.right.isDown) {
-          player.move("right");
+            player.move("right");
         }
-    
+
         if (controls.left.isDown) {
-          player.move("left");
+            player.move("left");
         }
-    
+
         if (controls.up.isDown) {
-          player.move("jump");
+            player.move("jump");
         }
-        
+
         if (controls.abilityOne.isDown) {
             player.callAbility(1);
         }
-        
+
         if (controls.abilityTwo.isDown) {
             player.callAbility(2);
         }
-        
-        
-        // EndOf checking player movement
-        
-    
-        
-    },
-    
-   
-}
 
+
+        // EndOf checking player movement
+
+
+
+    },
+
+
+}
