f49af0ab6432b6a183962df1931893199bdf3cdd
Gustaf <gustafolofsson93@gmail.com>
2016-09-18 21:03:49 +0000

    refactored code for creating objects from tilemaps.

--- cricket/src/states/Level.js
+++ cricket/src/states/Level.js
@@ -4,6 +4,7 @@ var map;
 var layer;
 // var backgroundLayer;
 
+var controls;
 var player;
 var abilityOneSprite;
 var abilityTwoSprite;
@@ -11,11 +12,15 @@ var textOne;
 var textTwo;
 
 // Groups and object readers
-var respawn;
+var spawnGroup;
 var coinGroup;
 var breakableGroup;
 var abilityGroup;
+var lethalGroup;
+var goalGroup;
+var groups; 
 
+/*
 var coinReader;
 var breakableReader;
 var abilityReader;
@@ -23,24 +28,12 @@ var groupReader;
 var singleCoin;
 var singleBreakable;
 var singleAbility;
-
+*/
 
 Game.Level.prototype = {
 
 
     create: function(game) {
-
-        //
-        groupReader = game.add.group();
-        coinReader = game.add.group();
-        coinGroup = game.add.group();
-        breakableReader = game.add.group();
-        breakableGroup = game.add.group();
-        abilityReader = game.add.group();
-        abilityGroup = game.add.group();
-
-
-
 
         // Initialize map and tilesets
         this.stage.backgroundColor = '#9CD5E2';
@@ -53,33 +46,96 @@ Game.Level.prototype = {
 
         layer.resizeWorld();
         map.setCollisionBetween(2, 4);
-
+        
+             //
+      //  groupReader = game.add.group();
+     //   coinReader = game.add.group();
+        coinGroup = game.add.group();
+     //   breakableReader = game.add.group();
+        breakableGroup = game.add.group();
+      //  abilityReader = game.add.group();
+        abilityGroup = game.add.group();
+        
+        //added this sunday
+        spawnGroup = game.add.group();
+        lethalGroup = game.add.group();
+        goalGroup = game.add.group();
+        
+        
         var objectLayers = ['spawn', 'pickups', 'breakable', 'lethalBlocks', 'supers', 'goal'];
 
-        var objectsInLayer = [
-            ['spawn'],
-            ['diamond', 'star'],
-            ['breakable'],
-            ['lava', 'water', 'cactus'],
-            ['highJump', 'longJump', 'stomp', 'shoot'],
+        var objectsInLayer = [  
+            ['spawn'], 
+            ['diamond', 'star'], 
+            ['breakable'], 
+            ['lava', 'water', 'cactus'], 
+            ['highJump', 'longJump', 'stomp', 'shoot'], 
             ['goal']
         ];
-
+        
+        
+        
+        // This is what I added sunday night instead of old way of creating objects.
+        // groups need to have the groups in the same order as the objectLayers array above for this solution to work.
+        groups = [spawnGroup, coinGroup, breakableGroup, lethalGroup, abilityGroup, goalGroup];
+      
+        
+        for (var i = 0; i < objectLayers.length; i++) {
+            groups[i].enableBody = true;
+            for (var j = 0; j < objectsInLayer[i].length; j++) {
+                
+                map.createFromObjects(objectLayers[i], objectsInLayer[i][j], objectsInLayer[i][j], 0, true, false, groups[i]);
+            }}
+        
+        /* TODO: Implement custom class? May be useful to connect the objects to their respective .js class file.
+        
+        // Phaser.Tilemap.createFromObjects(name, gid, key, frame, 
+                                    exists, autoCull, group, CustomClass, adjustY)
+        // arg object CustomClass (optional) = Phaser.Sprite
+            If you wish to create your own class, rather than Phaser.Sprite, 
+            pass the class here. Your class must extend Phaser.Sprite and have 
+            the same constructor parameters.
+        */
+        
+        
+        
+        /*
+        map.createFromObjects('spawn', 'spawn', 'spawn', 0, true, false, spawnGroup);
+        map.createFromObjects('pickups', 'diamond', 'diamond', 0, true, false, coinGroup);
+        map.createFromObjects('breakable', 'breakable', 'breakable', 0, true, false, breakableGroup);
+      
+       // var abilityList = ['highJump', 'longJump', 'stomp', 'shoot'];
+       // creates all ability blocks
+       for (var a = 0; a < objectsInLayer[4].length;a++ )
+            map.createFromObjects('supers', objectsInLayer[4][a], objectsInLayer[4][a], 0, true, false, abilityGroup);
+        
+       // create all different lethal blocks 
+        for (var a = 0; a < objectsInLayer[3].length;a++ )
+            map.createFromObjects('lethalBlocks', objectsInLayer[3][a], objectsInLayer[3][a], 0, true, false, lethalGroup);
+        
+        map.createFromObjects('goal', 'goal', 'goal', 0, true, false, goalGroup);
+        */
+       
+       // also added this sunday
+        // removes gravity and makes immovable for all objects created.
+        for (var g = 0; g < groups.length; g++) {
+            groups[g].forEach(function(item) {
+                item.body.allowGravity = false;
+                item.body.immovable = true;
+        }, this); 
+        }
         //;
-
-        //map.createFromObjects('breakable', 'breakable', 'breakable', 0, true, false, breakableGroup);
-
-        // Function to create all objects from the different object layers in Tiled. 
-        createAllObjects = function() {
+      /*
+         createAllObjects = function() {
             var temp;
-
+            
             for (var i = 0; i < objectLayers.length; i++) {
 
                 for (var j = 0; j < objectsInLayer[i].length; j++) {
                     map.createFromObjects(objectLayers[i], objectsInLayer[i][j], '', 0, true, false, groupReader)
                     groupReader.forEach(function(item) {
-
-
+                        
+                        
                         if (objectLayers[i] == 'spawn') {
                             console.log("Can't create spawn yet");
                         }
@@ -101,15 +157,17 @@ Game.Level.prototype = {
                         else if (objectLayers[i] == 'goal') {
                             console.log("Can't create goal yet");
                         }
-
+                   
+                        
                     });
                     groupReader.removeAll();
                 }
             }
-
-        };
-        createAllObjects();
-
+            
+       };
+        createAllObjects(objectLayers, objectsInLayer);
+        */
+        
 
         this.physics.arcade.gravity.y = 1000;
         var playerProperties = {
@@ -130,26 +188,28 @@ Game.Level.prototype = {
             abilityOne: this.input.keyboard.addKey(Phaser.Keyboard.Z),
             abilityTwo: this.input.keyboard.addKey(Phaser.Keyboard.X),
         };
-
+        
         // Shows current abilities
         abilityOneSprite = game.add.sprite(10, 500, player.abilityOne);
         abilityOneSprite.fixedToCamera = true;
-
+        
         abilityTwoSprite = game.add.sprite(100, 500, player.abilityTwo);
         abilityTwoSprite.fixedToCamera = true;
-
+        
         // Can also add style as last inparameter after (x, y, "text", 'style')
-        textOne = game.add.text(Math.floor(abilityOneSprite.x + abilityOneSprite.width / 2),
-            Math.floor(abilityOneSprite.y + abilityOneSprite.height / 2), "Z");
+        textOne = game.add.text(Math.floor(abilityOneSprite.x + abilityOneSprite.width/2), 
+                                Math.floor(abilityOneSprite.y + abilityOneSprite.height/2), "Z");
         textOne.fixedToCamera = true;
-
-        textTwo = game.add.text(Math.floor(abilityTwoSprite.x + abilityTwoSprite.width / 2),
-            Math.floor(abilityTwoSprite.y + abilityTwoSprite.height / 2), "X");
+        
+        textTwo = game.add.text(Math.floor(abilityTwoSprite.x + abilityTwoSprite.width/2), 
+                                Math.floor(abilityTwoSprite.y + abilityTwoSprite.height/2), "X");
         textTwo.fixedToCamera = true;
-
-
-
-
+    
+    
+        
+       
+        
+        
     },
 
 
@@ -158,10 +218,13 @@ Game.Level.prototype = {
         // Adding all collisions
         this.physics.arcade.collide(player, layer);
         this.physics.arcade.collide(player, breakableGroup);
+        
 
+        // Changed this when trying new way of creating coin, if reverting bring back the commented line.
         coinGroup.forEach(function(item) {
             game.physics.arcade.overlap(player, item, function() {
-                item.destroySprite()
+                //item.destroySprite()
+                item.kill();
             });
         }, this);
 
